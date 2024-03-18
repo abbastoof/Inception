545af3aac909c566ea78f9e77c0bbaf719faf815
abbastoof <abbas.toof@gmail.com>
2024-03-18 13:53:37 +0200

    editing compose file and entrypoint of wordpress

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -28,9 +28,9 @@ else
     && wp config create --path=/var/www/html --dbname="${MARIA_DB_DATABASE}" --dbuser="${MARIA_DB_USER}" --dbpass="${MARIA_DB_PASSWORD}" --dbhost="${MARIA_DB_HOST}" --force --allow-root \
     && wp core install --path=/var/www/html --url="${DOMAIN_NAME}" --title="${WORDPRESS_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --allow-root \
     && wp user create "${WORDPRESS_USER}" "${WORDPRESS_USER_EMAIL}" --role=author --user_pass="${WORDPRESS_USER_PASSWORD}" --path=/var/www/html --allow-root \
-    && wp theme install twentynineteen --activate --path=/var/www/html --allow-root \
     && wp option update siteurl "https://${DOMAIN_NAME}" --allow-root \
     && wp option update home "https://${DOMAIN_NAME}" --allow-root \
+    && wp theme install inspiro --activate --path=/var/www/html --allow-root \
     && echo "WordPress installation complete."
     touch /var/www/html/.wordpress_installed
 fi
